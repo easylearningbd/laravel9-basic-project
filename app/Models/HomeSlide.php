24487506fb1d32477f84e926f3ba095888d95cbc
easylearningbd <elebdsoft@gmail.com>
2022-03-15 19:52:27 +0600

    38 Backend Home Page Slider Option Part 1

---
+++ app/Models/HomeSlide.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomeSlide extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    // protected $fillable = [
+    //     'title',
+    //     'short_title',
+    //     'home_slide',
+    //     'video_url',
+    // ];
+
+
+}
+ 
