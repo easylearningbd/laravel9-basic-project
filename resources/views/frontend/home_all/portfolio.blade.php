dc211eb8b4a40eb38781333f9072729aa7aa28d4
easylearningbd <elebdsoft@gmail.com>
2022-03-17 23:58:29 +0600

    58 Fontend  Portfolio Show Setup Part 1

---
+++ resources/views/frontend/home_all/portfolio.blade.php
@@ -0,0 +1,46 @@
+<section class="portfolio">
+    <div class="container">
+    <div class="row justify-content-center">
+    <div class="col-xl-6 col-lg-8">
+    <div class="section__title text-center">
+    <span class="sub-title">04 - Portfolio</span>
+    <h2 class="title">All creative work</h2>
+    </div>
+    </div>
+    </div>
+   
+    </div>
+    <div class="tab-content" id="portfolioTabContent">
+  
+  @php
+$portfolio = App\Models\Portfolio::latest()->get();
+  @endphp
+     
+     
+    <div class="tab-pane show active" id="all" role="tabpanel" aria-labelledby="graphic-tab">
+    <div class="container">
+    <div class="row gx-0 justify-content-center">
+    <div class="col">
+    <div class="portfolio__active">
+
+        @foreach($portfolio as $item)
+    <div class="portfolio__item">
+    <div class="portfolio__thumb">
+        <img src="{{ asset($item->portfolio_image) }}" alt="">
+    </div>
+    <div class="portfolio__overlay__content">
+        <span>{{$item->portfolio_name}}</span>
+        <h4 class="title"><a href="portfolio-details.html">{{$item->  portfolio_title}}</a></h4>
+        <a href="portfolio-details.html" class="link">Case Study</a>
+    </div>
+    </div> 
+    @endforeach
+
+   
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
