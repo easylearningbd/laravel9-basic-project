ca157f2d18e20eaf280988381218340dd22c9aa1
easylearningbd <elebdsoft@gmail.com>
2022-03-20 16:08:36 +0600

    70 Fontend  Blog Show Setup Part 1

---
+++ resources/views/frontend/home_all/home_blog.blade.php
@@ -0,0 +1,36 @@
+ @php
+
+ $blogs = App\Models\Blog::latest()->limit(3)->get();
+
+@endphp
+
+
+ <section class="blog">
+                <div class="container">
+                    <div class="row gx-0 justify-content-center">
+
+    @foreach($blogs as $item)
+    <div class="col-lg-4 col-md-6 col-sm-9">
+        <div class="blog__post__item">
+            <div class="blog__post__thumb">
+                <a href="blog-details.html"><img src="{{ asset($item->blog_image) }} " alt=""></a>
+                <div class="blog__post__tags">
+      <a href="blog.html">{{ $item['category']['blog_category'] }}</a>
+                </div>
+            </div>
+            <div class="blog__post__content">
+                <span class="date">{{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }} </span>
+                <h3 class="title"><a href="blog-details.html"> {{ $item->blog_title }} </a></h3>
+                <a href="blog-details.html" class="read__more">Read mORe</a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+     
+                      
+                    </div>
+                    <div class="blog__button text-center">
+                        <a href="blog.html" class="btn">more blog</a>
+                    </div>
+                </div>
+            </section>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -316,60 +316,7 @@
             <!-- testimonial-area-end -->
 
             <!-- blog-area -->
-            <section class="blog">
-                <div class="container">
-                    <div class="row gx-0 justify-content-center">
-                        <div class="col-lg-4 col-md-6 col-sm-9">
-                            <div class="blog__post__item">
-                                <div class="blog__post__thumb">
-                                    <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb01.jpg" alt=""></a>
-                                    <div class="blog__post__tags">
-                                        <a href="blog.html">Story</a>
-                                    </div>
-                                </div>
-                                <div class="blog__post__content">
-                                    <span class="date">13 january 2021</span>
-                                    <h3 class="title"><a href="blog-details.html">Facebook design is dedicated to what's new in design</a></h3>
-                                    <a href="blog-details.html" class="read__more">Read mORe</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-9">
-                            <div class="blog__post__item">
-                                <div class="blog__post__thumb">
-                                    <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb02.jpg" alt=""></a>
-                                    <div class="blog__post__tags">
-                                        <a href="blog.html">Social</a>
-                                    </div>
-                                </div>
-                                <div class="blog__post__content">
-                                    <span class="date">13 january 2021</span>
-                                    <h3 class="title"><a href="blog-details.html">Make communication Fast and Effectively.</a></h3>
-                                    <a href="blog-details.html" class="read__more">Read mORe</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-sm-9">
-                            <div class="blog__post__item">
-                                <div class="blog__post__thumb">
-                                    <a href="blog-details.html"><img src="assets/img/blog/blog_post_thumb03.jpg" alt=""></a>
-                                    <div class="blog__post__tags">
-                                        <a href="blog.html">Work</a>
-                                    </div>
-                                </div>
-                                <div class="blog__post__content">
-                                    <span class="date">13 january 2021</span>
-                                    <h3 class="title"><a href="blog-details.html">How to increase your productivity at work - 2021</a></h3>
-                                    <a href="blog-details.html" class="read__more">Read mORe</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog__button text-center">
-                        <a href="blog.html" class="btn">more blog</a>
-                    </div>
-                </div>
-            </section>
+            @include('frontend.home_all.home_blog')
             <!-- blog-area-end -->
 
             <!-- contact-area -->
