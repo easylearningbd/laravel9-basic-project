d3af1a85cc6dd385fc7ffa2e778496356dbf2049
easylearningbd <elebdsoft@gmail.com>
2022-03-20 13:40:46 +0600

    63 Backend Blog Page Setup Part 1

---
+++ database/migrations/2022_03_20_073209_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('blog_category_id')->nullable();
+            $table->string('blog_title')->nullable();
+            $table->string('blog_image')->nullable();
+            $table->string('blog_tags')->nullable();
+            $table->text('blog_description')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
