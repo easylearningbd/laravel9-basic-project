730b12456ab7b0d9fdce119fab6e0adecd25dbc6
easylearningbd <elebdsoft@gmail.com>
2022-04-05 02:34:52 +0600

    86 Form Validation With JavaScript Part 2

--- resources/views/admin/blog_category/blog_category_add.blade.php
+++ resources/views/admin/blog_category/blog_category_add.blade.php
@@ -12,14 +12,14 @@
 
             <h4 class="card-title">Add Blog Category Page </h4> <br><br>
             
-            <form method="post" action="{{ route('store.blog.category') }}" >
+            <form method="post" id="myForm" action="{{ route('store.blog.category') }}" >
                 @csrf
 
                
 
             <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Blog Category Name</label>
-                <div class="col-sm-10">
+                <div class="form-group col-sm-10">
                     <input name="blog_category" class="form-control" type="text" id="example-text-input">
                     
                 </div>
@@ -44,6 +44,31 @@
 
 
 <script type="text/javascript">
+    $(document).ready(function (){
+        $('#myForm').validate({
+            rules: {
+                blog_category: {
+                    required : true,
+                }, 
+            },
+            messages :{
+                blog_category: {
+                    required : 'Please Enter Blog Category',
+                },
+            },
+            errorElement : 'span', 
+            errorPlacement: function (error,element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight : function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight : function(element, errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
     
 </script>
 
